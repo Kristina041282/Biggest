81d8852aa05283668846705a2d862716d7d43415
Kristina <kristi_7_82@inbox.ru>
2022-03-24 16:16:45 +0300

    CarPark

---
+++ Encapsulation/03_Immutable/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        CarPark carpark = new CarPark("Porsh", "picap");
+        carpark.print("Название машины: ");
+
+
+    }
+}
+
+
