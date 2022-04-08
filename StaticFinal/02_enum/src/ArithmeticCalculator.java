396f5e63cffc24e6cdf67713c3e355f0c5198eb4
Kristina <kristi_7_82@inbox.ru>
2022-04-08 10:19:21 +0300

    готово

--- StaticFinal/02_enum/src/ArithmeticCalculator.java
+++ StaticFinal/02_enum/src/ArithmeticCalculator.java
@@ -4,6 +4,7 @@ public class ArithmeticCalculator {
     private int num;
     public Operation type;//создала отдельную переменную. Для того чтобы enam заработал изменила
     // тип переменной int type на Operation type.
+    public int result = 0;
 
     public ArithmeticCalculator(int sum, int num, Operation type) {//создала конструктор с помощью которого
         //инициализировала переменную sum, num, type,и чтобы enam заработал изменила в параметрах конструктора
@@ -14,25 +15,27 @@ public class ArithmeticCalculator {
         calculate(type); //чтобы заработал метод calculate.
     }
 
-    public Operation getType() {//возвращает тип operation. Чтобы enam заработал изменила int getType
-        // на Operation getType.
-        return type;//чтобы возвращал тот или иной перечисляемый тип.
-    }
-
-    public void calculate(Operation type) {//метод калькулятора на вход которого
-        // передаётся одна из арифметических операций (объект класса Operation)
+    public int calculate(Operation type) {//метод калькулятора на вход которого передаётся одна из
+        // арифметических операций (объект класса Operation).
         switch (type) {
-            case ADD -> System.out.println(sum + num);
-
-            case MULTIPLY -> System.out.println(sum * num);
-
-            case SUBTRACT -> System.out.println(sum - num);
+            case ADD -> result = sum + num;
+            case MULTIPLY -> result = sum * num;
+            case SUBTRACT -> result = sum - num;
         }
-        return;
+        System.out.println(result);
+        return result;
+
     }
 }
-
-
+// public void calculate(Operation type) {//метод калькулятора на вход которого передаётся одна из
+//        // арифметических операций (объект класса Operation).
+//        switch (type) {
+//            case ADD -> System.out.println(sum + num);
+//            case MULTIPLY -> System.out.println(sum * num);
+//            case SUBTRACT -> System.out.println(sum - num);
+//        }
+//        return ; // Так прописывая этот метод получается просто печатаю в консоль результат расчета, то есть
+//        // не возвращаю. А нужно возвращать.
 
 
 
