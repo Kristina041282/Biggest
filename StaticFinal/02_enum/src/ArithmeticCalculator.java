427624801770d04e4b84d9aafc659e7f4c6d43ba
Kristina <kristi_7_82@inbox.ru>
2022-04-10 11:16:14 +0300

    Немогу вывести в консоль результат

--- StaticFinal/02_enum/src/ArithmeticCalculator.java
+++ StaticFinal/02_enum/src/ArithmeticCalculator.java
@@ -2,17 +2,15 @@ public class ArithmeticCalculator {
 
     private int sum;
     private int num;
-    public Operation type;//создала отдельную переменную. Для того чтобы enam заработал изменила
+    //public Operation type;//создала отдельную переменную. Для того чтобы enam заработал изменила
     // тип переменной int type на Operation type.
     public int result = 0;
 
-    public ArithmeticCalculator(int sum, int num, Operation type) {//создала конструктор с помощью которого
+    public ArithmeticCalculator(int sum, int num) {//создала конструктор с помощью которого
         //инициализировала переменную sum, num, type,и чтобы enam заработал изменила в параметрах конструктора
         //int type на Operation type.
         this.sum = sum;
         this.num = num;
-        this.type = type;
-        calculate(type); //чтобы заработал метод calculate.
     }
 
     public int calculate(Operation type) {//метод калькулятора на вход которого передаётся одна из
@@ -22,7 +20,6 @@ public class ArithmeticCalculator {
             case MULTIPLY -> result = sum * num;
             case SUBTRACT -> result = sum - num;
         }
-        System.out.println(result);
         return result;
 
     }
--- StaticFinal/02_enum/src/Main.java
+++ StaticFinal/02_enum/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(7, 4, Operation.MULTIPLY);
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(7, 4);
+
+        arithmeticCalculator.calculate(Operation.ADD); // название класса.далее идет название вызываемого
+        // метода и в круглых скобках (тип операции который вызываем);
+
+        arithmeticCalculator.calculate(Operation.MULTIPLY);
+
+        arithmeticCalculator.calculate(Operation.SUBTRACT);
 
     }
 }
