b755c79c5dd0e1d55588fe814b58210088944c93
Kristina <kristi_7_82@inbox.ru>
2022-04-21 18:51:07 +0300

    готово.

--- StaticFinal/03_computer/src/Main.java
+++ StaticFinal/03_computer/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
         Computer computer = new Computer("Japan", "sony");
 
-        computer.setProcessor(new Processor(5));
+        computer.setProcessor(new Processor(5)); // (создала здесь в классе main комплектующие и добавила
+        // в computer). Для того чтобы в консоль вышло toString нужно, чтобы заработали сеттеры для всех комплектующих
+        // компьютера, которые создала в классе computer.
         computer.setInformationAccumulator(new InformationAccumulator(5, TypeInformationAccumulator.HDD));
         computer.setRAM(new RAM(5));
         computer.setScreen(new Screen(5, TypeScreen.VA));
